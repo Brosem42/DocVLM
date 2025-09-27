8056fde07d47f0bd4dd6c4db9a7abb9ca60289c3
Brianna <brosem42@gmail.com>
2025-09-26 23:00:38 -0500

    Update model path

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ stop_words = set(stopwords.words('english'))
 stemmer = PorterStemmer()
 
 #load fine tuned model + tokenizer
-model_name = 'pytorch_model.bin'
+model_name = './bert_company_model'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 label_encoder = pickle.load(open('label_encoder.pkl', 'rb'))
