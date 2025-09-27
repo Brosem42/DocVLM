e144de8fc8cf5f266e1afd14e990e7d0d87da727
Brianna <brosem42@gmail.com>
2025-09-26 21:30:11 -0500

    Fix spacing

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import streamlit as st # type: ignore
 #ui design
 st.title("VLM Document Classifier with Bert")
 st.write("" \
-"This project uses a vision language model to classify PDF documents for business transactions occurring within enterprise environments." \
- 
+"This project uses a vision language model to classify PDF documents for business transactions occurring within enterprise environments. " \
 "The model is trained on a custom dataset of business documents, including invoices, purchase orders, shipping orders, inventory, etc." \
 ""
 )
