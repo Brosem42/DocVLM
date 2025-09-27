38de300c75414fefcef39f1249da810c9c0f30ac
Brianna <brosem42@gmail.com>
2025-09-27 01:07:36 -0500

    upd

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import streamlit as st # type: ignore
 import PyPDF2 # type: ignore
 from transformers import AutoTokenizer, AutoModelForSequenceClassification # type: ignore
 import torch #type: ignore
-import fitz #type: ignore
 import numpy as np # type: ignore
-import re
 import pickle
 from nltk.corpus import stopwords #type: ignore
 from nltk.stem import PorterStemmer #type: ignore
