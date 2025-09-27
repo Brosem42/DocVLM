ddb3b95c0f77427d9be2f81dc2c7debf1c11e84e
Brianna <brosem42@gmail.com>
2025-09-26 21:25:14 -0500

    Uploaded file logic

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 
 # Page title
+import streamlit as st # type: ignore
+
+#ui design
+st.title("VLM Document Classifier with Bert")
+st.write("This project uses a vision language model to classify PDF documents for business transactions occurring within enterprise environments.")
+st.write("The model is trained on a custom dataset of business documents, including invoices, purchase orders, shipping orders, inventory, etc.")
+
+#file upload
+uploaded_file = st.file_uploader("Choose a PDF file to upload", type=["pdf"])
+
+#uploaded file condition 
+#meaning if someone has uploaded a file, then go ahead and do something, if someone does not upload a file, then we can give an error message or any message
+if uploaded_file is not None:
+    pass 
